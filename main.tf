709e44e0911a723af3f328f05b61bf31df5c512c
hezijie <lonegunmanb@hotmail.com>
2024-02-27 16:54:00 +0800

    trigger post push

--- main.tf
+++ main.tf
@@ -15,9 +15,9 @@ resource "azurerm_cognitive_account" "this" {
   custom_question_answering_search_service_key = var.custom_question_answering_search_service_key
   local_auth_enabled                           = var.local_auth_enabled
   dynamic_throttling_enabled                   = var.dynamic_throttling_enabled
+  metrics_advisor_super_user_name              = var.metrics_advisor_super_user_name
   fqdns                                        = var.fqdns
   metrics_advisor_aad_client_id                = var.metrics_advisor_aad_client_id
-  metrics_advisor_super_user_name              = var.metrics_advisor_super_user_name
   metrics_advisor_website_name                 = var.metrics_advisor_website_name
   outbound_network_access_restricted           = var.outbound_network_access_restricted
   public_network_access_enabled                = var.public_network_access_enabled
